fd17c2a084b222874740fc69cb37a295aac8eefa
bdulhodiy <muhiddinovabdulhodiy777@gmail.com>
2025-05-08 18:38:13 +0500

    Yangi o'zgarishlar

--- randomchat/asgi.py
+++ randomchat/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import video.routing  # Make sure this imports your WebSocket routes
+import video.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'randomchat.settings')
 
